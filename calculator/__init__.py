1924776872f1f604c1ba0a617a98f99193b2b99c
rithika955 <rk955@njit.edu>
2024-09-26 18:15:13 -0400

    new fixes

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,8 +1,14 @@
-'''this is an init method'''
+'''my calc'''
 def add(a,b):
     '''add method'''
     return a + b
-
 def subtract(a,b):
     '''subtract method'''
     return a - b
+def multiply(a,b):
+    '''multiply method'''
+    return a*b
+def divide(a,b):
+    '''division method'''
+    return a/b
+
