c94ca7f840a6fea5c09ea33c8bfc7aa3e3f8f582
rithika955 <rk955@njit.edu>
2024-10-01 16:35:13 -0400

    added code to implement faker

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -2,18 +2,9 @@
 """
 This module contains tests for the calculator operations and Calculation class.
 
-The tests are designed to verify the correctness of basic arithmetic operations
-(addition, subtraction, multiplication, division) implemented in the calculator.operations module,
-as well as the functionality of the Calculation class that encapsulates these operations.
 """
 
-# Import statements:
-# Disable specific pylint warnings that are not relevant for this test file.
-# Import the Decimal class for precise decimal arithmetic, which is especially useful in financial calculations.
-# Import pytest for writing test cases.
-# Import the Calculation class from the calculator package to test its functionality.
-# Import the arithmetic operation functions (add, subtract, multiply, divide) to be tested.
-# pylint: disable=unnecessary-dunder-call, invalid-name
+
 from decimal import Decimal
 import pytest
 from calculator.calculation import Calculation
